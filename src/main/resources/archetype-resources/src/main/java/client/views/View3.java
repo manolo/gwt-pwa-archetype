8ba8283a6bdee9580777f9f58f6b5d6f8eba6ec3
Manolo Carrasco <manolo@vaadin.com>
2016-11-12 03:02:55 +0100

    Adding a grid for the TODOs

--- src/main/resources/archetype-resources/src/main/java/client/views/View3.java
+++ src/main/resources/archetype-resources/src/main/java/client/views/View3.java
@@ -3,7 +3,9 @@
 #set( $symbol_escape = '\' )
 package ${package}.client.views;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.shared.GWT;
+import com.google.gwt.dom.client.FormElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -11,13 +13,28 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
+import com.vaadin.polymer.Polymer;
 import com.vaadin.polymer.paper.widget.PaperButton;
 import com.vaadin.polymer.paper.widget.PaperInput;
 import com.vaadin.polymer.paper.widget.PaperTextarea;
 import com.vaadin.polymer.paper.widget.PaperToast;
 import com.vaadin.polymer.vaadin.widget.VaadinDatePicker;
+import com.vaadin.polymer.vaadin.widget.VaadinGrid;
+
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
 
 public class View3 extends Composite {
+
+  @JsType(isNative = true)
+  public interface Todo {
+    @JsProperty String getDate();
+    @JsProperty String getTitle();
+    @JsProperty String getDescription();
+    @JsProperty void setDate(String d);
+    @JsProperty void setTitle(String d);
+    @JsProperty void setDescription(String d);
+  }
 
   interface View3UiBinder extends UiBinder<HTMLPanel, View3> {
   }
@@ -29,16 +46,30 @@ public class View3 extends Composite {
   @UiField PaperTextarea descr;
   @UiField VaadinDatePicker date;
   @UiField PaperToast toast;
+  @UiField VaadinGrid grid;
+  @UiField FormElement form;
 
   public View3() {
     initWidget(viewUi.createAndBindUi(this));
     toast.setFitInto(this.getElement());
+    grid.setItems(JavaScriptObject.createArray());
   }
 
   @UiHandler("submit")
   void onClick(ClickEvent e) {
-    toast.setText("Date: " + date.getValue() + " Title: " + title.getValue() + " Description: " + descr.getValue());
-    toast.open();
+    if (date.getValue().isEmpty()) {
+      date.setInvalid(date.getValue().isEmpty());
+      return;
+    }
+
+    if (title.validate() && descr.validate()) {
+      Todo todo = (Todo) JavaScriptObject.createObject();
+      todo.setDate(date.getValue());
+      todo.setTitle(title.getValue());
+      todo.setDescription(descr.getValue());
+      Polymer.apply(grid.getElement(), "push", "items", todo);
+      form.reset();
+    }
   }
 
 }
